94d21c7f7ab6b7479497060793947047ee34f527
Hannah Austin <hannah.austin@bp.com>
2023-03-08 18:33:27 +0000

    update styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ApolloProvider } from "@apollo/client";
+import apolloClient from "../lib/apollo";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,26 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import prisma from "../lib/prisma";
+import { gql, useQuery } from "@apollo/client";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const RICK_COUNT = gql`
+  query {
+    rickCount {
+      id
+    }
+  }
+`;
+
 export default function Home() {
-  // const [rickCount, setRickCount] = useState(0);
-  // console.log(prisma);
-  // const getRickCount = async () => {
-  //   const rickEntries = await prisma.rickcount.findMany();
-  //   console.log(rickEntries);
-  //   setRickCount(rickEntries.length);
-  // };
-  // useEffect(() => {
-  //   getRickCount();
-  // }, []);
-  // console.log(rickCount);
+  const { data, loading, error } = useQuery(RICK_COUNT);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Oh no... {error.message}</p>;
+
   return (
     <>
       <Head>
@@ -27,22 +30,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <button
-          type="submit"
-          className="group relative flex w-100 justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          Add to Rick Count
-        </button>
+        <div className="flex-column">
+          <button
+            type="submit"
+            className="group relative flex w-100 justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Add to Rick Count
+          </button>
+          <p className="mt-4">
+            Current Rick Roll Count: {data.rickCount.length}
+          </p>
+        </div>
       </main>
     </>
   );
 }
-
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  const rickEntries = await prisma.rickcount.findMany();
-  const rickCount = { count: rickEntries.length };
-  console.log(rickCount.count);
-  return {
-    props: rickCount,
-  };
-};
